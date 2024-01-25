81804d14b22e3367d1cd0af031a8adb71754ce16
Scott Harp <ssharp9999@gmail.com>
2024-01-25 15:15:59 -0800

    mongodb connection console log

--- tracker-rest/server.mjs
+++ tracker-rest/server.mjs
@@ -9,10 +9,22 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI);
 
+// Connect to to the database
+const db = mongoose.connection;
+
+// Open event is called when the database connection successfully opens
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+/* ROUTES START */
+
 // Routes to register/login/logout
 app.post('/register', register);
 app.post('/login', login);
 app.post('/logout', logout);
+
+/* ROUTES END */
 
 // Listen on the PORT
 const port = process.env.PORT || 3000;
