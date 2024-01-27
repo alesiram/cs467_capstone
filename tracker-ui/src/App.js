83712b5d3cfe6cf3fd1204c3cd495de441d90dac
daviesa2 <daviesa2@oregonstate.edu>
2024-01-26 20:00:09 -0800

    Added home page and styled like login

--- tracker-ui/src/App.js
+++ tracker-ui/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/home/home';
 import LoginPage from './pages/login/login';
 
 function App() {
   return (
     <div className="App">
       <Router>
+          {/* Route for the Home Page */}
+          <Route path="/" exact component={HomePage} />
           <Route path='/login'>
             <LoginPage />
           </Route>
